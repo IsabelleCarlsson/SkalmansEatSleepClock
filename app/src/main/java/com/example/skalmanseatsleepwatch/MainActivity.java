152c7656b216d5099b9fa4b8a05ebc3b5b718ef7
IsabelleCarlsson <isabelle.carlsson@my.jcu.edu.au>
2019-03-21 18:46:40 +1000

    Update drawables, add settings and To Do text function

--- app/src/main/java/com/example/skalmanseatsleepwatch/MainActivity.java
+++ app/src/main/java/com/example/skalmanseatsleepwatch/MainActivity.java
@@ -1,27 +1,48 @@
 package com.example.skalmanseatsleepwatch;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AnalogClock;
-import android.widget.Toast;
+import android.text.format.Time;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    final Handler handler = new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // inititate the analog clock
-        AnalogClock simpleAnalogClock = findViewById(R.id.simpleAnalogClock);
-
-        // perform click event on analog clock
-        simpleAnalogClock.setOnClickListener(new View.OnClickListener() {
+        handler.post(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Analog Clock", Toast.LENGTH_SHORT).show(); // display a toast for analog clock
+            public void run() {
+                updateTimeToDo();
+                handler.postDelayed(this, 1000);
             }
         });
     }
+
+    public void updateTimeToDo() {
+        TextView to_do = findViewById(R.id.to_do);
+        Time now = new Time();
+        now.setToNow();
+        int mins = now.minute;
+
+        // Update time to do text according to minutes
+        if ((mins >= 7 && mins <= 12) || (mins >= 17 && mins <= 21) || (mins >= 28 && mins <= 33) || (mins >= 38 && mins <= 43) || (mins >= 48 && mins <= 53)) {
+            to_do.setText(R.string.sleep);
+        } else if ((mins >= 0 && mins <= 6) || (mins >= 13 && mins <= 16) || (mins >= 34 && mins <= 37) || (mins >= 54 && mins <= 59)) {
+            to_do.setText(R.string.eat);
+        } else {
+            to_do.setText(R.string.drink);
+        }
+        (findViewById(R.id.vertical_layout)).invalidate();
+    }
+
+//    public void settingsClicked(View view) {
+//        Intent intent = // Something here
+//        startActivityForResult(intent, SettingsActivity.SETTING_REQUEST);
+//    }
 }
